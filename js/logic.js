e94bf9ad5b7baa342f996e29a0935f2d6bd945d3
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-17 20:57:00 -0400

    styled

--- js/logic.js
+++ js/logic.js
@@ -18,3 +18,14 @@ var map = L.map("mapid", {
 
 maplayer.addTo(map);
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson")
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+}
